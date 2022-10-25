d0c22b0170d1e8b5eec97274626ecbe94e1d3795
john <kiokoj81@gmail.com>
2022-10-25 21:59:24 +0300

    Updated the number of movies to be displayed at a go

--- app/getshows.js
+++ app/getshows.js
@@ -1,26 +1,35 @@
 const shows = document.querySelector('.shows');
+const more = document.getElementById('more');
 
-const getshows = async () => {
+const getShows = async () => {
   const res = await fetch('https://api.tvmaze.com/shows');
   const data = await res.json();
 
+  showData(data);
+};
+
+const showData = (data) => {
   let show = '';
 
+  let count = 0;
+
   data.forEach((e) => {
-    console.log(e);
-    show += `<div class="my-show">
-      <div class="my-show-image">
-     <img src="${e.image.medium}"></img>
-  </div>
-      <div class="my-show-title">
-          ${e.name}
+    if (count < 6) {
+      show += `<div class="my-show">
+            <div class="my-show-image">
+          <img src="${e.image.medium}"></img>
+          </div>
+              <div class="my-show-title">
+                  ${e.name}
 
-          <i class="bi bi-suit-heart"></i>
-      </div>
-  </div>`;
+                  <i class="bi bi-suit-heart"></i>
+              </div>
+          </div>`;
+    }
+
+    count++;
   });
-
   shows.innerHTML = show;
 };
 
-getshows();
+getShows();
